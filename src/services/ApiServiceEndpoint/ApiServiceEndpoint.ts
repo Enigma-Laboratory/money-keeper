7cff532798e03d4accd215a6134140b2b8a2320c
phanvohieunghia <phvohieunghia@gmail.com>
2024-05-15 19:22:09 +0700

    fix: api route

--- src/services/ApiServiceEndpoint/ApiServiceEndpoint.ts
+++ src/services/ApiServiceEndpoint/ApiServiceEndpoint.ts
@@ -1,7 +1,7 @@
 export class ApiServiceEndPoint {
   protected endPoint: string;
   constructor() {
-    // this.endPoint = 'http://103.169.35.190:1337' || 'http://localhost:1337';
-    this.endPoint = 'http://localhost:1337';
+    this.endPoint = 'http://103.169.35.190:1337';
+    // this.endPoint = 'http://localhost:1337';
   }
 }
