77825dd8325ee46486d2b051a61be48b10e5f371
TuTuanLe <tutuanletkpt@gmail.com>
2024-05-28 17:18:51 +0700

    fix: improve http client service

---
+++ src/utils/constants.ts
@@ -0,0 +1,24 @@
+export const ROUTE_PATH = '';
+
+export const EVENT_NAME = {
+  OPEN_MODAL: 'event_name_open_modal',
+  CLOSE_MODAL: 'event_name_close_modal',
+};
+
+// THC: TABLE_HEIGHT_CONFIG
+export const THC = {
+  HEADER_HEIGHT: 64,
+  PADDING_MAIN_LAYOUT_HEIGHT: 14,
+  ORDER_PAGE: {
+    HEADER_HEIGHT: 78,
+    HEADER_MARGIN: 10,
+    PAGINATION_HEIGHT: 32,
+    PAGINATION_MARGIN: 16,
+    HEADER_TABLE: 55,
+  },
+};
+
+export const DEFAULT_ROOM_NAME = 'money-keeper';
+export const TOKEN_KEY = 'money-keeper-auth';
+export const REFRESH_TOKEN_KEY = 'money-keeper-auth-refresh';
+export const USER_IDENTITY = 'user-identity-auth';
