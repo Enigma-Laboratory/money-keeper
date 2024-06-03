1e910de69c67876f0395ec8aebaa8d6089117a44
TuTuanLe <tutuanletkpt@gmail.com>
2024-06-03 22:04:50 +0700

    chore: remove unused code

--- src/services/http/httpConfig.ts
+++ src/services/http/httpConfig.ts
@@ -2,28 +2,20 @@ export const HttpConfig = {
   AUTH: 'auth',
   ORDERS: 'orders',
   PRODUCTS: 'products',
-  ORDER_DETAILS: 'order-details',
   USERS: 'users',
   OPERATIONAL_SETTINGS: 'operational-settings',
 };
 
 export const HttpConfigOrder = {
-  GET_ONE_ORDER: 'get-one-order',
-  CREATE_ORDER: 'create-order',
-  DELETE_ORDER: 'delete-order',
-  UPDATE_ORDER: 'update-order',
   UPDATE_ORDER_STATUS: 'order-status',
   UPDATE_ORDER_STATUSES: 'order-statuses',
 };
 
 export const HttpConfigAuth = {
   REFRESH_TOKEN: 'refresh-token',
-  GET_ONE_USER: 'get-one-user',
   SIGN_IN: 'sign-in',
   SIGN_UP: 'sign-up',
   SIGN_OUT: 'sign-out',
-  UPDATE_USER: 'update-user',
-  DELETE_USER: 'delete-user',
   FORGOT_PASSWORD: 'forgot-password',
 };
 
