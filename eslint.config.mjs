dd07e48ea38336789d14bb550bea3958d02a78f0
Tu Tuan Le <tutuanletkpt@gmail.com>
2024-10-13 13:45:40 +0700

    fix: eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,46 @@
+import eslint from '@eslint/js';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config({
-  extends: [
-    'next/core-web-vitals',
-    'plugin:react-hooks/recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/recommended',
-    'prettier',
-  ],
-  settings: {
-    'import/resolver': {
-      typescript: true,
-      node: true,
+export default tseslint.config(
+  eslint.configs.recommended,
+
+  {
+    linterOptions: {},
+    settings: {
+      'import/resolver': {
+        typescript: true,
+        node: true,
+      },
+    },
+    overrides: [],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 2024,
+        sourceType: 'module',
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    ignores: ['/public', '/build', '/node_modules'],
+    files: ['/src/**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+    plugins: ['react', '@typescript-eslint', 'import'],
+    rules: {
+      semi: ['error', 'always'],
+      'import/named': 'off',
+      'import/namespace': 'off',
+      'import/default': 'off',
+      'import/no-named-as-default-member': 'off',
+      'import/no-unresolved': 'off',
+      'react/prop-types': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
     },
   },
-  overrides: [],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./packages/*/tsconfig.json'],
-  },
-  plugins: ['react', '@typescript-eslint', 'import'],
-  rules: {
-    semi: ['error', 'always'],
-    'import/named': 'off',
-    'import/namespace': 'off',
-    'import/default': 'off',
-    'import/no-named-as-default-member': 'off',
-    'import/no-unresolved': 'off',
-    'react/prop-types': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn',
-  },
-});
+);
